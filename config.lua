d1f33a1254f6b1cea2ba8d74a8a7528b07beefa1
DeeBee <159756433+danik1804@users.noreply.github.com>
2025-07-13 19:34:15 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Target system: 'qtarget', 'ox_target'
+Config.TargetSystem = 'qtarget'
+
+Config.Locations = {
+    { name = "Hlavní Garáže", coords = vector3(221.2, -789.4, 29.6), heading = 337.1 },
+    { name = "LSPD Stanice", coords = vector3(407.5, -988.4, 28.1), heading = 230.4 },
+    { name = "Autoškola", coords = vector3(243.5, -1416.2, 29.4), heading = 144.7 },
+    { name = "Banka", coords = vector3(152.6, -1027.3, 28.2), heading = 249.6 },
+    { name = "Radnice", coords = vector3(-515.8, -263.9, 34.3), heading = 296.2 },
+}
